413bdf282dff917840ba6d5c01978470f9939797
JorgeRearteCar <jorge.rearte@flybondi.com>
2024-11-03 12:09:13 -0300

    add app products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Category, SubCategory, Supplier
 
-# Register your models here.
+@admin.register(Supplier)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name','cuit','address','phone','email')
+    search_fields = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock', 'category', 'subcategory', 'origin', 'supplier', 'get_supplier_products')
+
+    def get_supplier_products(self, obj):
+        return ", ".join([supplier_product.name for supplier_product in obj.supplier_products.all()])
+    get_supplier_products.short_description = "Supplier Products"  
--- products/models.py
+++ products/models.py
@@ -1,10 +1,48 @@
 from django.db import models
 
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)  # Este es el campo ID
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    cuit = models.CharField(max_length=11, unique=True)  # CUIT único de 11 caracteres
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    PRODUCT_ORIGIN_CHOICES = [
+        ('OWN', 'Propio'),
+        ('SUP', 'Proveedor'),
+    ]
+
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    origin = models.CharField(
+        max_length=3,
+        choices=PRODUCT_ORIGIN_CHOICES,
+        default='OWN'
+    )
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, null=True, blank=True)
+    
+    supplier_products = models.ManyToManyField('self', symmetrical=False, blank=True, related_name="related_products")
+    
+
+    def __str__(self):
+        return f"{self.name} ({self.get_origin_display()})"
